802a21f217169c6c7aa7c920c69903c98558dbcd
yeyezaw <zawyehtut@outlook.com>
2018-11-02 18:49:27 +0630

    UPDATE - new contents

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from the site 'You Site Name'"; //letter subject
+	$to = 'zawyehtut@outlook'; // Enter Your E-mail
+	$from = 'admin@you-site-name.com'; // Admin e-mail
+	$fromName = 'Your Company Name'; // Your company name
+	$charset = 'UTF-8';
+?>
