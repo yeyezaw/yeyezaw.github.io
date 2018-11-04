aec73a6367fe25d4955d1e0ca19fbc933ceff7c9
yeyezaw <zawyehtut@outlook.com>
2018-11-04 20:54:05 +0630

    ADD - email server

--- mailer/form_setting.php
+++
@@ -1,8 +0,0 @@
-<?php
-	/*Form settings*/
-	$subj = "New message from the site 'You Site Name'"; //letter subject
-	$to = 'zawyehtut@outlook'; // Enter Your E-mail
-	$from = 'admin@you-site-name.com'; // Admin e-mail
-	$fromName = 'Your Company Name'; // Your company name
-	$charset = 'UTF-8';
-?>
